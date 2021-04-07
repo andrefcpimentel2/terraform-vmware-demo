0b9a6af5088cf01d9d403fd464b39c8eea2d5d10
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2021-04-07 10:20:45 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,6 +12,10 @@ variable "vmtemp" {
   description = "Name of the template available in the vSphere"
 }
 
+variable "storage_policy" {
+  description = "Name of the Storage Policy used in the vSphere"
+}
+
 variable "data_disk_size_gb" {
   description = "Storage data disk size size"
   default     = 20
