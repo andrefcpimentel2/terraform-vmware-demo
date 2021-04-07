d308e5093ae6ce492f762d9d5258d979e2978b05
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2021-04-07 09:48:14 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 terraform {
-  required_version = "0.14.3-bundle2021022512"
-  required_providers {
-    nsxt = {
-      source  = "vmware/nsxt"
-      version = "=3.1.1"
-    }
-  }
+  required_version = "~>0.13"
 }
